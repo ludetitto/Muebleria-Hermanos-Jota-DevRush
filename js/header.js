6ce7370890df9d30655c0506e30ba83cf5a81e3d
Lucas Duran <lucasduran510@gmail.com>
2025-09-05 14:18:26 -0300

    cambio de color dinamico del texto del header

--- js/header.js
+++ js/header.js
@@ -3,8 +3,39 @@ function toggleMenu() {
   navContainer.classList.toggle("active");
 }
 
-document.addEventListener("click", function (event) {
+function updateHeaderLinksColor() {
+  if (window.innerWidth > 768) {
+    const header = document.querySelector("header");
+    const sections = document.querySelectorAll("[data-bg]");
+    const headerHeight = header.offsetHeight;
+
+    let currentSection = null;
+
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      //Si el header se esta interponiendo con una seccion
+      if (rect.top <= headerHeight && rect.bottom >= 0) {
+        currentSection = section;
+      }
+    });
+
+    if (currentSection) {
+      const bgType = currentSection.getAttribute("data-bg");
+      if (bgType === "light") {
+        header.classList.add("on-light-section");
+      } else {
+        header.classList.remove("on-light-section");
+      }
+    }
+  }
+}
+
+/*document.addEventListener("click", function (event) {
   if (event.target.tagName === "A" && window.innerWidth <= 768) {
     document.querySelector(".nav-container").classList.remove("active");
   }
-});
+});*/
+
+window.addEventListener("scroll", updateHeaderLinksColor);
+
+updateHeaderLinksColor();
