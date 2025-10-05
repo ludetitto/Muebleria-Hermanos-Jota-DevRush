6b82290dcddc060a0b427d82ba64561aa50723d4
Lucas Duran <lucasduran510@gmail.com>
2025-10-05 13:50:46 -0300

    usar <ProductList> en Catalog

--- client/src/pages/Catalog.jsx
+++ client/src/pages/Catalog.jsx
@@ -5,14 +5,20 @@ import ProductDetail from "../components/ProductDetail";
 import useProductos from "../hooks/useProductos";
 import Banner from "../components/Banner";
 
-export default function Catalog({ onSelectProducto, onVolver, onAgregarAlCarrito }) {
+export default function Catalog({
+  onSelectProducto,
+  onVolver,
+  onAgregarAlCarrito,
+}) {
   const [search, setSearch] = useState("");
   const [selected, setSelected] = useState(null);
 
   const { productos, loading, error } = useProductos();
 
   const productosFiltrados = useMemo(() => {
-    return productos.filter((p) => p.nombre.toLowerCase().includes(search.toLowerCase()));
+    return productos.filter((p) =>
+      p.nombre.toLowerCase().includes(search.toLowerCase())
+    );
   }, [productos, search]);
 
   return (
@@ -37,11 +43,12 @@ export default function Catalog({ onSelectProducto, onVolver, onAgregarAlCarrito
             ) : error ? (
               <p className="error">Error al cargar los productos</p>
             ) : (
-              <div className="grid">
-                {productosFiltrados.map((prod) => (
-                  <ProductCard key={prod.id} producto={prod} onSelect={() => onSelectProducto ? onSelectProducto(prod) : setSelected(prod)} />
-                ))}
-              </div>
+              <ProductList
+                productos={productosFiltrados}
+                onSelect={(prod) =>
+                  onSelectProducto ? onSelectProducto(prod) : setSelected(prod)
+                } // cuando clickeas pasa a detalle
+              />
             )}
           </section>
         </main>
