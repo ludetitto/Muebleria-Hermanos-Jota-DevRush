20f6e5c269e6628b4e2dc7ff3570c17f1400a86d
maticoppes <matiasareguaticoppes@gmail.com>
2025-10-27 10:47:38 -0300

    Conexión a MongoDB Atlas + CRUD Product con Mongoose

--- backend/src/routes/productos.js
+++ backend/src/routes/productos.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/Product"); // importamos el modelo Mongoose
 
 const productos = require("../data/data");
 
